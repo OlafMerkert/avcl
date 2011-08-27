4644539ba5547cb58a110e196301e07053985eec
Olaf Merkert <olaf@m-merkert.de>
2011-08-27 10:58:53 +0200

    Protokoll zum Erzeugen von Formularen festgelegt

--- models.lisp
+++ models.lisp
@@ -33,11 +33,58 @@
         (print-unreadable-object (,g!object ,g!stream :type t)
           (with-slots ,slot-names ,g!object
             (declare (ignorable ,@slot-names))
-            ,@(mapcar #`(princ ,a1 ,g!stream) print-form)))))))
+            ,@(mapcar #`(princ ,a1 ,g!stream) print-form))))
+      ,(generate-create-form name slots)
+      ,(generate-edit-form name slots slot-names))))
+
+(defun generate-create-form (name slots)
+  (let ((parameter-slots (filter (lambda (slot) (if (member :parameter slot) (first slot))) slots)))
+   (with-gensyms!
+     `(define-form ,(symb name '-create-form) ,(format nil "~:(~A~) anlegen" name)
+          (,@parameter-slots)           ; Parameter
+        ,slots                          ; Felder
+        ;; Buttons
+        (("Speichern && Weiter" (lambda (,g!alist)
+                                  (apply #'make-instance ',name ,g!alist)))
+         ("Speichern" (lambda (,g!alist)
+                        (prog1
+                            (apply #'make-instance ',name ,g!alist)
+                          (close-form))))
+         ("Abbrechen" (lambda (,g!alist)
+                        (declare (ignorable ,g!alist))
+                        (close-form))))
+        ;; keine Initialisierung
+        ))))
+
+(defun generate-edit-form (name slots slot-names)
+  (with-gensyms!
+    `(define-form ,(symb name '-edit-form) ,(format nil "~:(~A~) bearbeiten" name)
+       (,name)                    ; Parameter
+         ,slots                         ; Felder
+         ;; Buttons
+         (("Speichern" (lambda (,g!alist)
+                         (let ((,g!object (assoc1 ',name ,g!alist)))
+                           ,@(mapcar (lambda (slot-name)
+                                       `(setf (slot-value ,g!object ',slot-name)
+                                              (assoc1 ',slot-name ,g!alist)))
+                                     slot-names)
+                           (close-form))))
+          ("Abbrechen" (lambda (,g!alist)
+                        (declare (ignorable ,g!alist))
+                        (close-form))))
+         ;; Initialisierung
+         ,@(mapcar (lambda (slot-name)
+                     `(setf (form-value ',slot-name)
+                            (slot-value ,name ',slot-name)))
+                   slot-names))))
+
+;; braucht aufrufe für close-form, form-value (setf-bar)
+;; Button click hat closure mit alist der felder einträge aufzurufen
+(defmacro define-form (name title parameters fields buttons &body init))
 
 (defclass/q taetigkeit
     ((titel string)
-     (bedarf integer)
+     (bedarf integer :default 2)
      (dozent string)
      (bereich (from "Grundstudium"
                     "Aufbaustudium"
@@ -52,14 +99,14 @@
   (name))
 
 (defclass/q wunsch
-    ((assistent assistent)
-     (taetigkeit taetigkeit)
+    ((assistent :parameter)
+     (taetigkeit (from-collection :taetigkeiten))
      (staerke (from 1 2 3 4)))
   ((name assistent) " wuenscht " (titel taetigkeit) " [W" staerke "]"))
 
 (defclass/q zuweisung
-    ((assistent assistent)
-     (taetigkeit taetigkeit)
+    ((assistent  (from-collection :assistenten))
+     (taetigkeit (from-collection :taetigkeiten))
      (score integer :default 0)
      (fest boolean))
   ((name assistent) " uebernimmt " (titel taetigkeit) (if fest " [fest]" "")))
