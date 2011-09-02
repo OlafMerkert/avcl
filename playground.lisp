1dd69fc9cee6ead2aa655ecaf781a2d1774e7225
Olaf Merkert <olaf@m-merkert.de>
2011-09-02 10:46:48 +0200

    adjust for changed model

--- playground.lisp
+++ playground.lisp
@@ -10,17 +10,19 @@
              (multiple-value-bind (col new) (collection key)
                (when new
                  (add inst col)))))
-    (let ((olaf (make-instance 'assistent :name "Olaf Merkert"))
-          (algebra (make-instance 'taetigkeit
+    (let* ((olaf (make-instance 'assistent :name "Olaf Merkert"))
+           (algebra (make-instance 'taetigkeit
                           :titel "Algebra"
                           :dozent "David Masser"
                           :bedarf 2
-                          :bereich "Aufbaustudium")))
-      (put :assistenten olaf)
-      (put :taetigkeiten algebra)
-      (put :wuensche (make-instance 'wunsch :assistent olaf
+                          :bereich "Aufbaustudium"))
+           (wunsch (make-instance 'wunsch :assistent olaf
                                     :taetigkeit algebra
                                     :staerke 1))
-      (put :zuweisungen (make-instance 'zuweisung :assistent olaf
-                                       :taetigkeit algebra
-                                       :score 39129)))))
+           (zuweisung (make-instance 'zuweisung ;wunsch wunsch
+                                    :score 39129) ))
+      (put :assistenten olaf)
+      (put :taetigkeiten algebra)
+      (put :wuensche wunsch)
+      ;; todo
+      (put :zuweisungen zuweisung))))
